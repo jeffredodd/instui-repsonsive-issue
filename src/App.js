541bcb4cc27cda8f2d468a23590f3d84c967d078
Jeffrey Johnson <jeffrey.johnson@instructure.com>
2022-02-03 12:28:14 -0800

    Importing InstUI Responsive

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {Responsive} from '@instructure/ui-responsive'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Responsive
+        match="media"
+        query={{
+          mobile: {maxWidth: '767px'},
+          tablet: {minWidth: '768px'},
+          desktop: {minWidth: '1024px'}
+        }}
+        props={{
+          mobile: {
+            info: 'Mobile'
+          },
+          tablet: {
+            info: 'Tablet'
+          },
+          desktop: {
+            info: 'Desktop'
+          }
+        }}
+        render={props => <div>Hello World {props.info}</div>}
+      />
     </div>
   );
 }
